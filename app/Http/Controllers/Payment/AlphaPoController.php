ce4db640c8946ac60672ac1fb63453002abfbe76
artempobedennyi <artem.pobedennyi309@gmail.com>
2023-12-18 16:18:11 +0900

    Add logs on AlphaPo controller

--- app/Http/Controllers/Payment/AlphaPoController.php
+++ app/Http/Controllers/Payment/AlphaPoController.php
@@ -39,8 +39,12 @@ class AlphaPoController extends Controller
                     $alphaPo = new AlphaPo;
                     $price = $alphaPo->getCryptoPrice($currency);
 
+                    Log::info("Price of " . $currency . " => " $price);
+
                     if (!is_null($price)) {
                         $pay_amount = floatval($amount) * floatval($price);
+
+                        Log::info("Total withrawal amount => " . $pay_amount);
                     
                         Txn::update(
                             $transaction->tnx, 
@@ -81,8 +85,12 @@ class AlphaPoController extends Controller
                     $alphaPo = new AlphaPo;
                     $price = $alphaPo->getCryptoPrice($currency);
 
+                    Log::info("Price of " . $currency . " => " $price);
+
                     if (!is_null($price)) {
                         $pay_amount = floatval($amount) * floatval($price);
+
+                        Log::info("Total withrawal amount => " . $pay_amount);
                         
                         Txn::update(
                             $transaction->tnx, 
