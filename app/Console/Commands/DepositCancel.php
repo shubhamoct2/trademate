eb2e80f6786dbaecbb6dfa16df56fd13efd3b800
artempobedennyi <artem.pobedennyi309@gmail.com>
2024-01-08 03:53:33 +0900

    Update cron job functions for logs

--- app/Console/Commands/DepositCancel.php
+++ app/Console/Commands/DepositCancel.php
@@ -37,7 +37,7 @@ class DepositCancel extends Command
     public function handle()
     {
         $now = Carbon::now();
-        // Log::info('CronJob (DepositCancel) => started at: ' . $now->format('Y-m-d H:i:s'));
+        Log::info('CronJob (DepositCancel) => started at: ' . $now->format('Y-m-d H:i:s'));
 
         $oneHourBefore = Carbon::now('Europe/Berlin')->subHours(1);
         // Log::info('CronJob (DepositCancel) => checking time: ' . $oneHourBefore->format('Y-m-d H:i:s'));
@@ -56,7 +56,7 @@ class DepositCancel extends Command
             Log::info('CronJob (DepositCancel) => cancelled ' . $pending_deposit_count . ' deposit');
         }
 
-        // Log::info('CronJob (DepositCancel) => finished at: ' . $now->format('Y-m-d H:i:s'));
+        Log::info('CronJob (DepositCancel) => finished at: ' . $now->format('Y-m-d H:i:s'));
 
         return Command::SUCCESS;
     }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
             ->runInBackground(); 
 
         $schedule->command('deposit:cancel')
-            ->everyFiveMinutes()
+            ->hourly()
             ->runInBackground(); 
     }
 
